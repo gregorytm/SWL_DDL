7b84e7163315edf07e4f745dc0320ef1800d3396
Greg Marsh <gregmarsh@Gregs-iMac.local>
2020-10-31 13:05:35 -0700

    outser_space changeas

--- outer_space.sql
+++ outer_space.sql
@@ -5,16 +5,7 @@ DROP DATABASE IF EXISTS outer_space;
 
 CREATE DATABASE outer_space;
 
-\c outer_space
-
-CREATE TABLE planets (
-  id SERIAL PRIMARY KEY,
-  name TEXT NOT NULL,
-  orbital_period_in_years FLOAT NOT NULL,
-  orbits_around INTEGER REFERENCES orbits,
-  galaxy INTEGER REFERENCES galaxy,
-  moons INTEGER REFERENCES moons
-);
+\c outer_space;
 
 CREATE TABLE orbits (
   id SERIAL PRIMARY KEY,
@@ -31,14 +22,14 @@ CREATE TABLE moons (
   moons_list TEXT[]
 );
 
-INSERT INTO planets (name, orbital_period_in_years, orbits_around, galaxy, moons)
-VALUES
-('Earth', 1.00, 1, 1, 1),
-('Mars', 1.88, 1,1,2),
-('Venus', 0.62, 1,1,3),
-('Neptune', 164.8, 1,1,4),
-('Proxima Centauri b', 11.0, 2, 1, 5),
-('Gliese 876 b', 61.0, 3 ,1 ,6);
+CREATE TABLE planets (
+  id SERIAL PRIMARY KEY,
+  name TEXT NOT NULL,
+  orbital_period_in_years FLOAT NOT NULL,
+  orbits_around INTEGER REFERENCES orbits,
+  galaxy INTEGER REFERENCES galaxy,
+  moons INTEGER REFERENCES moons
+);
 
 INSERT INTO orbits (orbits_name)
 VALUES
@@ -58,3 +49,12 @@ VALUES
 ('{"Naiad", "Thalassa", "Despina", "Galatea", "Larissa", "S/2004 N 1", "Proteus", "Triton", "Nereid", "Halimede", "Sao", "Laomedeia", "Psamathe", "Neso"}'),
 ('{}'),
 ('{}');
+
+INSERT INTO planets (name, orbital_period_in_years, orbits_around, galaxy, moons)
+VALUES
+('Earth', 1.00, 1, 1, 1),
+('Mars', 1.88, 1,1,2),
+('Venus', 0.62, 1,1,3),
+('Neptune', 164.8, 1,1,4),
+('Proxima Centauri b', 11.0, 2, 1, 5),
+('Gliese 876 b', 61.0, 3 ,1 ,6);
